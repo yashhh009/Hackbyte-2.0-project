ff56e1787749cde4b3cb1fc820511b4468e7319d
Yash Gupta <yash.gupta.csiot21@ggits.net>
2024-04-06 01:30:11 +0530

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -1,56 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
-from sklearn.metrics import mean_absolute_error
 
+def train_arima_model(data):
+    train_size = int(len(data) * 0.8)
+    train_data, test_data = data[:train_size], data[train_size:]
+    model = ARIMA(train_data['CO'], order=(5,1,0))
+    model_fit = model.fit()
+    return model_fit
 
-# In[13]:
+def make_predictions(model, test_data):
+    predictions = model.forecast(steps=len(test_data))
+    return predictions
 
+def predict_increase(data_file_path):
+    sensor_data = pd.read_csv(data_file_path)
+    model = train_arima_model(sensor_data)
+    test_data = sensor_data[train_size:]
+    predictions = make_predictions(model, test_data)
+    
+    if not predictions.empty:
+        if not test_data.empty:
+            last_observed_value = test_data['CO'].iloc[-1]
+            predicted_value = predictions.iloc[-1]
 
-sensor_data = pd.read_csv('D:\da iot practicals\CO(ppm).csv')
-
-
-# In[14]:
-
-
-train_size = int(len(sensor_data) * 0.8)
-train_data, test_data = sensor_data[:train_size], sensor_data[train_size:]
-
-
-# In[15]:
-
-
-model = ARIMA(train_data['CO'], order=(5,1,0))
-model_fit = model.fit()
-
-
-# In[16]:
-
-
-predictions = model_fit.forecast(steps=len(test_data))
-
-
-# In[17]:
-
-
-if not predictions.empty:
-    if not test_data.empty:
-        last_observed_value = test_data['CO'].iloc[-1]
-        predicted_value = predictions.iloc[-1]
-
-        if predicted_value > last_observed_value:
-            print("There is a possibility of increase in CO")
+            if predicted_value > last_observed_value:
+                return "There is a possibility of increase in CO"
+            else:
+                return "No significant increase in CO predicted"
         else:
-            print("No significant increase in CO predicted")
+            return "No testing data available. Unable to make predictions."
     else:
-        print("No testing data available. Unable to make predictions.")
-else:
-    print("No predictions available. Unable to make predictions.")
-
-
-# In[ ]:
+        return "No predictions available. Unable to make predictions."
